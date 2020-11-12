14c0d0548d9ee8062e65c421978b00c9f31d9fa6
unknown <rarbtk10@gmail.com>
2020-11-12 05:59:36 -0300

    se agrego el archivo para la creacion de nuevos productos

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ let session = require("express-session");
 var indexRouter = require("./routes/index");
 var userRouter = require("./routes/user");
 var productRouter = require("./routes/product");
+var productCreation = require("./routes/creation");
 
 
 
@@ -32,6 +33,8 @@ require("dotenv").config();
 app.use("/", indexRouter);
 app.use("/product", productRouter);
 app.use("/", userRouter);
+app.use("/creation",productCreation);
+
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
